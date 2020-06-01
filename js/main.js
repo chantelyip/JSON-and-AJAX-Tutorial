6bd07059457dc190449d93425e622f9fafff6b9d
Chantel Yip <chantelyip422@gmail.com>
2020-06-01 00:23:57 -0700

    created single variable that contains all of the data...also known as creating a JSON

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-var myCat = {
-    "name" : "Meowsalot",
+var thePets = [
+{
+    "name": "Meowsalot",
     "species": "cat",
     "favFood": "tuna"
+},
+
+{   "name" : "Barky",
+    "species" : "dog",
+    "favFood" : "carrots"
 }
-
-var myFavColors = ["blue", "green", "purple"];
-
+]
