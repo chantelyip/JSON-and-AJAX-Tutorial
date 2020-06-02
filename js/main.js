79aa369fe192e58f647889a22b79d328ee68afdb
Chantel Yip <chantelyip422@gmail.com>
2020-06-01 22:37:17 -0700

    added json filter to main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-//example of JSON: objects in arrays, nested inside of each other
-var thePets = [
-{
-    "name": "Meowsalot",
-    "species": "cat",
-    "favFood": "tuna"
-},
-
-{   "name" : "Barky",
-    "species" : "dog",
-    "favFood" : "carrots"
-}
-]
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[10]);
+};
+ourRequest.send();
